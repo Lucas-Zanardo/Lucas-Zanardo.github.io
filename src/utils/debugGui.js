33e81bb7c6bc7f205d777170fcbab6bdf4c0c605
Lucas Zanardo <lucas.zanardo38@gmail.com>
2024-08-30 15:30:20 +0200

    Added method to display vector2 in gui addons

--- src/utils/debugGui.js
+++ src/utils/debugGui.js
@@ -1,6 +1,14 @@
 
-export function addVector3(gui, object, propertyName) {
-    const folder = gui.addFolder(propertyName);
+
+export function addVector2(gui, object, propertyName, customName = '') {
+    const folder = gui.addFolder(customName || propertyName);
+    folder.add(object[propertyName], 'x').listen();
+    folder.add(object[propertyName], 'y').listen();
+    return folder;
+}
+
+export function addVector3(gui, object, propertyName, customName = '') {
+    const folder = gui.addFolder(customName || propertyName);
     folder.add(object[propertyName], 'x').listen();
     folder.add(object[propertyName], 'y').listen();
     folder.add(object[propertyName], 'z').listen();
